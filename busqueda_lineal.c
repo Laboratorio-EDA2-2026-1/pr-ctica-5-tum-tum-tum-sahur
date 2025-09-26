031b87a73110880c537f4df36f7cb6a846f7aa94
miliyam1 <yamiliruiz91@gmail.com>
2025-09-26 08:43:43 -0600

    Update busqueda_lineal.c

--- busqueda_lineal.c
+++ busqueda_lineal.c
@@ -1,1 +1,29 @@
 // Escribe aquí la implementación de los ejercicios de búsqueda lineal.
+
+//EJERCICIO 1
+#include <stdio.h>
+
+int busquedaLineal(int A[], int n, int k) {
+	for (int i = 0; i < n; i ++) {
+		if (A[i] == k) {
+			return 1;
+		} //retorna el indice donde se encuentra k
+	}
+	return -1; //si no se encuentra
+}
+
+int main() {
+	int A[] = {5, 3, 8, 6, 7};
+	int n = sizeof(A) / sizeof(A[0]);
+	int k = 6;
+
+	int result = busquedaLineal(A, n, k);
+
+	if (result != -1){
+		printf("Elemento %d encontrado en el indice: %d\n", k, result);
+	} else{
+		printf("El elemnto %d no encontrado en el arrreglo.\n", k);
+	} 
+
+	return 0;
+}
